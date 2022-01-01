82966add1489841eae8d485d959ca2da31638719
HongJinHyeok <redmax45@naver.com>
2022-01-01 16:49:46 +0900

    design-pattern init

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  plugins: ['prettier'],
+  extends: ['prettier', 'plugin:prettier/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};

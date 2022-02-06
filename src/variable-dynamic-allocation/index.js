b07f943fd35e1060513cf44f4ac33befbd56152e
HongJinHyeok <redmax45@naver.com>
2022-02-06 22:35:53 +0900

    동적변수 예제

--- src/variable-dynamic-allocation/index.js
+++ src/variable-dynamic-allocation/index.js
@@ -2,15 +2,21 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 
 let object = {};
-const languages = ['javascript', 'typescript'];
+// const languages = ['javascript', 'typescript'];
 
-const dynamic_variable = () => {
-  return new Promise(resolve => {
-    for (const language of languages) {
-      object[`${language}`] = `${language}`;
-    }
-    console.log(object.javascript);
-  });
-};
+// const dynamic_variable = () => {
+//   return new Promise(resolve => {
+//     for (const language of languages) {
+//       object[`${language}`] = `${language}`;
+//     }
+//     console.log(object.javascript);
+//   });
+// };
 
-dynamic_variable();
+// dynamic_variable();
+
+for (let i = 0; i < 10; i++) {
+  object[`variable${i}`] = 'test';
+}
+
+console.log(object);

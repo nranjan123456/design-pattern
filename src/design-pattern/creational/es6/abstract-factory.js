95711129594adea1e32c4bb771375571c70aa0ac
HongJinHyeok <redmax45@naver.com>
2022-02-22 23:42:15 +0900

    추상 팩토리

--- src/design-pattern/creational/es6/abstract-factory.js
+++ src/design-pattern/creational/es6/abstract-factory.js
@@ -13,14 +13,16 @@ function pilotDroidFactory() {
   return new Rx24();
 }
 
-function B1() {}
-B1.prototype.info = function () {
-  return 'B1, Battle Droid';
-};
+class B1 {
+  info() {
+    return 'B1, Battle Droid';
+  }
+}
 
-function Rx24() {}
-Rx24.prototype.info = function () {
-  return 'Rx24, Pilot Droid';
-};
+class Rx24 {
+  info() {
+    return 'Rx24, Pilot Droid';
+  }
+}
 
-module.exports = droidProducer;
+export default droidProducer;

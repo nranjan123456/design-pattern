70d1482bd582942c20fbea39242a39c29c656bf2
HongJinHyeok <redmax45@naver.com>
2022-02-23 00:01:50 +0900

    빌더 패턴

---
+++ src/design-pattern/creational/builder.js
@@ -0,0 +1,32 @@
+'use strict';
+
+function Request() {
+  this.url = '';
+  this.method = '';
+  this.payload = {};
+}
+
+function RequestBuilder() {
+  this.request = new Request();
+
+  this.forUrl = function (url) {
+    this.request.url = url;
+    return this;
+  };
+
+  this.useMethod = function (method) {
+    this.request.method = method;
+    return this;
+  };
+
+  this.payload = function (payload) {
+    this.request.paylaod = payload;
+    return this;
+  };
+
+  this.build = function () {
+    return this.request;
+  };
+}
+
+module.exports = RequestBuilder;

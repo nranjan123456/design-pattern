06ff77f22463984faaddcd0673fcdb48232c4d43
HongJinHyeok <redmax45@naver.com>
2022-04-02 14:44:35 +0900

    computed property name example

---
+++ src/computed-property-name/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const funcName0 = 'func0';
+const funcName1 = 'func1';
+const funcName2 = 'func2';
+
+const obj = {
+  [funcName0]() {
+    return 'func0';
+  },
+  [funcName1]() {
+    return 'func1';
+  },
+  [funcName2]() {
+    return 'func2';
+  },
+};
+
+for (let i = 0; i < 3; i++) {
+  console.log(obj['func' + i]());
+}
